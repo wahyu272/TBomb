193c1707f52f7f29c35e548bd2b83f745aaa47bf
wahyu272 <sakuraroyal08@gmail.com>
2025-07-20 21:39:29 +0700

    Update TBomb.sh

--- TBomb.sh
+++ TBomb.sh
@@ -39,7 +39,7 @@ banner() {
     fi
     echo -e "\e[1;34m For Any Queries Join Me!!!\e[0m"
     echo -e "\e[1;32m           Telegram: https://t.me/TBombChat \e[0m"
-    echo -e "\e[4;32m   YouTube: https://www.youtube.com/c/SpeedXTech \e[0m"
+    echo -e "\e[4;32m   YouTube: https://www.youtube.com/@wahyumod \e[0m"
     echo " "
     echo "NOTE: Kindly move to the PIP version Of TBomb for more stability."
     echo " "
